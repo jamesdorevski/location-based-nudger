198f9830e733081c28616055292fceb985555c6e
James Dorevski <jdorevski@gmail.com>
2020-05-28 14:18:16 +1000

    moved db setup into new file

---
+++ js/dbSetup.js
@@ -0,0 +1,38 @@
+let createTable = (db) => {
+    
+    db.run('CREATE TABLE ipAddresses (ip VARCHAR(15));');
+};
+
+let populateTable = (db) => {
+    
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.65");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.66");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.67");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.68");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.69");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.70");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.71");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.72");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.73");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.74");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.75");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.76");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.77");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.78");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.79");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.80");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.81");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.82");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.83");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.84");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.85");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.86");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.87");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.88");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.89");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.90");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.91");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.92");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.93");');
+    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.94");');
+};
--- js/popup.js
+++ js/popup.js
@@ -9,7 +9,8 @@ let getIpButton = document.getElementById("getIP").onclick = function() {
 
 let compareIpAddress = (publicIP) => {
 
-    initaliseDb(publicIP);
+    let atCampus = initaliseDb(publicIP);
+    displayIfAtCampus(atCampus); 
 };
 
 let initaliseDb = (publicIP) => {
@@ -23,56 +24,20 @@ let initaliseDb = (publicIP) => {
 
         createTable(db);
         populateTable(db);
+        
         let atCampus = determineIfAtCampus(db, publicIP);
-        displayIfAtCampus(atCampus);
+        return atCampus;
     });    
-};
-
-let createTable = (db) => {
-    
-    db.run('CREATE TABLE ipAddresses (ip VARCHAR(15));');
-};
-
-let populateTable = (db) => {
-    
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.65");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.66");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.67");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.68");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.69");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.70");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.71");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.72");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.73");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.74");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.75");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.76");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.77");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.78");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.79");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.80");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.81");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.82");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.83");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.84");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.85");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.86");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.87");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.88");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.89");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.90");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.91");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.92");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.93");');
-    db.run('INSERT INTO ipAddresses(ip) VALUES ("203.10.91.94");');
 };
 
 let determineIfAtCampus = (db, publicIP) => {
 
     let result = db.prepare("SELECT COUNT(ip) AS count FROM ipAddresses WHERE ip LIKE '%"+ publicIP +"%' ORDER BY ip ASC;");
     result.step();
+
     // debug
     console.log(result.getAsObject().count);
+    
     return result.getAsObject().count;
 };
 
