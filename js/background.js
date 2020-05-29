8fe04259846e65601cda8897e2d75923827c6396
James Dorevski <jdorevski@gmail.com>
2020-05-29 15:17:04 +1000

    made page redirect when going on fb

--- js/background.js
+++ js/background.js
@@ -1,4 +1,17 @@
 chrome.runtime.onInstalled.addListener(function() {
 
+    let pattern = "*://*.facebook.com/*";
 
+    chrome.webRequest.onBeforeRequest.addListener(
+        redirect,
+        {urls: [pattern] },
+        ["blocking"]
+    );
+
+    function redirect(requestDetails) {
+        console.log("Redirecting: " + requestDetails.url);
+        return {
+          redirectUrl: "chrome-extension://fdcianmgcblpcphklfimgniikdolhpfm/html/nudge-site.html"
+        };
+      }
 });
