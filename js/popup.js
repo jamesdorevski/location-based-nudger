f2a9e1841de0707b4216afdfaa62ecb591714e1d
James Dorevski <jdorevski@gmail.com>
2020-05-29 11:07:19 +1000

    fixed db return bug

--- js/popup.js
+++ js/popup.js
@@ -9,25 +9,33 @@ let getIpButton = document.getElementById("getIP").onclick = function() {
 
 let compareIpAddress = (publicIP) => {
 
-    let atCampus = initaliseDb(publicIP);
-    displayIfAtCampus(atCampus); 
+    // returns a promise that you need to retrieve the result of (the db)
+    let dbPromise = initaliseDb();
+    
+    dbPromise.then(db => {
+        
+        let atCampus = determineIfAtCampus(db, publicIP);
+        displayIfAtCampus(atCampus);
+    });
 };
 
-let initaliseDb = (publicIP) => {
+let initaliseDb = () => {
+    
     config = {
         locateFile: (file) => `../lib/sqljs-wasm/${file}`
     };
 
-    initSqlJs(config).then(function(SQL) {
+    let dbPromise = initSqlJs(config).then(function(SQL) {
 
         let db = new SQL.Database();
-
+        
         createTable(db);
         populateTable(db);
-        
-        let atCampus = determineIfAtCampus(db, publicIP);
-        return atCampus;
-    });    
+
+        return db;
+    }); 
+
+    return dbPromise;
 };
 
 let determineIfAtCampus = (db, publicIP) => {
@@ -35,9 +43,6 @@ let determineIfAtCampus = (db, publicIP) => {
     let result = db.prepare("SELECT COUNT(ip) AS count FROM ipAddresses WHERE ip LIKE '%"+ publicIP +"%' ORDER BY ip ASC;");
     result.step();
 
-    // debug
-    console.log(result.getAsObject().count);
-    
     return result.getAsObject().count;
 };
 
@@ -45,7 +50,7 @@ let displayIfAtCampus = (atCampus) => {
 
     let displayCampusDiv = document.getElementById("atCampusDiv");
 
-    if (atCampus === 1) {
+    if (atCampus >= 1) {
         displayCampusDiv.innerHTML = "You're at the UOW campus.";
     } else {
         displayCampusDiv.innerHTML = "You're not at the UOW campus.";
