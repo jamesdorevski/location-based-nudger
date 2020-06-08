59a3cd29452594d7f77483e5d4f159fe3972b59f
James Dorevski <jdorevski@gmail.com>
2020-06-08 15:05:10 +1000

    added nudge-site css, modernised functions

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,5 +1,5 @@
 // tells background that user has left nudged site to reset nudge 
-window.addEventListener('unload', function(event) {
+window.addEventListener('unload', () => {
     chrome.runtime.sendMessage({offNudgeSite: 1});
 });
 
--- js/nudge-site.js
+++ js/nudge-site.js
@@ -1,20 +1,24 @@
 let originalUrl;
 
-document.getElementById("acknowledgeButton").addEventListener("click", function() {
+document.getElementById("backButton").addEventListener("click", () => {
+    window.history.back();
+});
+
+document.getElementById("acknowledgeButton").addEventListener("click", () => {
     window.location.replace(originalUrl);
 });
 
 // recieve the original url once nudge-site has completed loaded 
-window.addEventListener('load', function() {
-    this.console.log("page has completed loaded");
+window.addEventListener('load', () => {
+    console.log("page has completed loaded");
 
-    chrome.runtime.sendMessage({nudgeLoaded: 1}, function(response) {
+    chrome.runtime.sendMessage({nudgeLoaded: 1}, (response) => {
         console.log("response recieved:" + response.url);
         originalUrl = response.url;
     });
 });
 
 // notify background that user went back 
-window.addEventListener('unload', function(event) {
+window.addEventListener('unload', () => {
     chrome.runtime.sendMessage({offNudgeSite: 1});
 });
