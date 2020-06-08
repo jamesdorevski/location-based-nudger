6836775da718c6590cff4a16f8e8c35bc5f9fb8b
James Dorevski <jdorevski@gmail.com>
2020-06-08 15:18:36 +1000

    edited popup css and formatting

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 let isAtUni;
 let redirectedUrl;
-let onNudgeSite = 0; 
+let onNudgeSite = 0;
 let facebookUrl = '*://*.facebook.com/*';
 let redditUrl = '*://*.reddit.com/*';
 
-// init sqllite db 
-chrome.runtime.onInstalled.addListener(function() {
+// init sqllite db
+chrome.runtime.onInstalled.addListener( () => {
     
     initaliseDb();
 	let dbPromise = getDb();
@@ -16,44 +16,38 @@ chrome.runtime.onInstalled.addListener(function() {
 	});
 });
 
-chrome.webRequest.onBeforeRequest.addListener(
-    redirectUrl, 
-    { urls: [ facebookUrl, redditUrl ] }, 
-    ["blocking"]
-);
+chrome.webRequest.onBeforeRequest.addListener(redirectUrl, { urls: [ facebookUrl, redditUrl ] }, [ 'blocking' ]);
 
 function redirectUrl(requestDetails) {
     
-    console.log("is at uni: " + isAtUni);
-    console.log("is on nudge site: " + onNudgeSite);
+    console.log('is at uni: ' + isAtUni);
+	console.log('is on nudge site: ' + onNudgeSite);
 
-    if (onNudgeSite == 0) {
-        if (isAtUni == 1) {
-        
-            console.log('Redirecting: ' + requestDetails.url);
-            redirectedUrl = requestDetails.url;
+	if (onNudgeSite == 0) {
+		if (isAtUni == 1) {
+			console.log('Redirecting: ' + requestDetails.url);
+			redirectedUrl = requestDetails.url;
 
-            // flag so user can continue to site once confirmed
-            onNudgeSite = 1; 
+			// flag so user can continue to site once confirmed
+			onNudgeSite = 1;
 
-            return {
-                redirectUrl: 'chrome-extension://' + chrome.runtime.id + '/html/nudge-site.html'
-            };
-        }
-    }
-};
-
+			return {
+				redirectUrl: 'chrome-extension://' + chrome.runtime.id + '/html/nudge-site.html'
+			};
+		}
+	}
+}
 
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    if (request.nudgeLoaded === 1) {
-        console.log("nudge site loaded");
-        sendResponse({url: redirectedUrl});
-    }
+	if (request.nudgeLoaded === 1) {
+		console.log('nudge site loaded');
+		sendResponse({ url: redirectedUrl });
+	}
 });
 
 chrome.runtime.onMessage.addListener( (request) => {
-    if (request.offNudgeSite == 1) {
-        console.log("confirmed user has left nudged site");
-        onNudgeSite = 0;
-    }
+	if (request.offNudgeSite == 1) {
+		console.log('confirmed user has left nudged site');
+		onNudgeSite = 0;
+	}
 });
